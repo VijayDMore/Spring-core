11691f29cadfb0ac874b11f5f1538d97f15bedcb
VijayDMore <vijaymore5377@gmail.com>
2023-01-05 14:05:57 +0530

    first commit

---
+++ src/main/java/com/jsp/demopackage/AppPenDriver.java
@@ -0,0 +1,13 @@
+package com.jsp.demopackage;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppPenDriver {
+	public static void main(String[] args) {
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("myspring.xml");
+		Pen pen=(Pen)applicationContext.getBean("mypen");
+		pen.Write();
+	}
+
+}
---
+++ src/main/java/com/jsp/demopackage/Pen.java
@@ -0,0 +1,10 @@
+package com.jsp.demopackage;
+
+public class Pen {
+	Pen(){
+	   System.out.println("constructor loading");
+ }
+	public void Write() {
+		System.out.println("checking");
+	}
+}
